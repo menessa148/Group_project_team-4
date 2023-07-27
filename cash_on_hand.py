698b59deeb76dedc31a7a7ee0ba5efd7af984d68
Menessa <menessa.chung17@gmail.com>
2023-07-27 14:00:22 +0800

    cash on hand

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,2 +1,39 @@
-print("hello world")
-print("hello")
+from pathlib import Path
+import csv
+
+# Define the file path
+fp = Path.cwd()/"csv_reports"/"cash on hand.csv.csv"
+
+# Open the CSV file and read its contents
+with fp.open(mode= "r", encoding="UTF-8") as file:
+    reader = csv.reader(file)
+
+    # Skip the header row
+    next(reader)
+
+    # Create a list to store the "Day" ans "Cash on hand" values
+    coh = []
+
+    # Read each row in the CSV file and append the "Day" and "Cash on hand"
+    for row in reader:
+        coh.append([row[0], row[1]])
+
+# Define a function to calculate deficits for each day
+def calculate_deficit(coh):
+    deficits = []
+
+    # Calculate the deficit for each day
+    for current in range(1, len(coh)):
+        prev_day_cash = int(coh[current - 1][1])
+        current_day_cash = int(coh[current][1])
+        deficit = prev_day_cash - current_day_cash
+        deficits.append(deficit)
+
+    return deficits
+
+# Call the function to calculate deficits and store the results in deficits_list
+deficits_list = calculate_deficit(coh)
+
+# Print the deficits for each day
+for day, deficit in enumerate(deficits_list):
+    print(f"Day {day + 1}, Deficit: {deficit}")
---
+++ overheads.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+import csv
+
+# Creation of overhead_function function
+def overhead_function():
+    """
+    - Function will give the highest overhead of the business over the 90 days
+    - No parameter required
+    - ????
+    """ 
+
+    # Assign "overheads-day-90.csv" file located in the current working directory to variable file_path
+    file_path = Path.cwd()/"csv_reports"/"overheads-day-90.csv"
+
+    # Open the file in read mode with UTF-8 encoding and no newline conversion
+    with file_path.open(mode = "r",encoding = "UTF-8", newline="") as file:
+        
+        # Create a csv.reader object to read the file
+        reader = csv.reader(file)
+        
+        # Skip the header row in the CSV file
+        next(reader)
+    
+    # Create an empty list to store category and overhead data
+        overheads_list = []
+
+        # Iterate through each row in the CSV file
+        for row in reader:
+
+            # Convert the second element of the row to float type and append 
+            # the category and overhead as a list to overheads_list
+            overheads_list.append([row[0],float(row[1])])
+
+    # Create an empty dictionary to store category as keys and overhead values as values
+    overheads_dict = {}
+
+    # Iterate through each item in overheads_list
+    for category, overheads in overheads_list:
+
+        # Check if the category already exists in the dictionary
+        if category in overheads_dict:
+            # If the category exists, update the value with the maximum of the current overhead and the existing value
+            overheads_dict[category] = max(overheads_dict[category], overheads)
+        else:
+            # If the category doesn't exist, add a new entry to the dictionary with the current overhead value
+            overheads_dict[category] = overheads
+    
+    # Find the highest overhead value from the dictionary values
+    highest_number = max(overheads_dict.values())
+
+    # Iterate through the dictionary items
+    for category, value in overheads_dict.items():
+         # Check if the overhead value matches the highest overhead value
+        if value == highest_number:
+            # Return a formatted string indicating the category with the highest overhead value
+            return f"[HIGHEST OVERHEAD] {category.upper()}: {highest_number}%"
+
+# print overhead_function
+print(overhead_function())
